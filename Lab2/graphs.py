284879faad1b48f19320006a26136e9e2eb22a69
jonathannstorm <117719484+jonathannstorm@users.noreply.github.com>
2023-11-20 10:29:13 +0100

    Graph klass klar, Weighted påbörjad

--- Lab2/graphs.py
+++ Lab2/graphs.py
@@ -1,17 +1,23 @@
 
-
+def main():
+    wg = WeightedGraph()
+    wg.add_vertex(1)
 
 
 class Graph:
     def __init__(self, startVertex=None, valuesDictionary = None, directed=False):
         self._adjacencyList = {}
-        if valuesDictionary is None:
-            valuesDictionary = {}
         self._valuelist = valuesDictionary
         self._isdirected = directed
+        if valuesDictionary is None:
+            valuesDictionary = {}
+        if startVertex is not None:
+            self._adjacencyList[startVertex] = []
+        
+
 
     def vertices(self):
-        return self._adjacencyList.keys()
+        return self._valuelist.keys()
     
 
     def edges(self):
@@ -23,21 +29,50 @@ class Graph:
         return edges
 
 
-
     def neighbours(self,v):
-        
+        neighboutingVertices = self._adjacencyList[v]
+        return neighboutingVertices
 
 
     def add_edge(self,a,b):
+        self.add_vertex(a)
+
+        if b not in self._adjacencyList[a]:
+            self._adjacencyList[a].append(b)
+
+        if b not in self._valuelist.keys():
+            self.set_vertex_value(b, None)
+
+
     def add_vertex(self,a):
+        if a not in self._valuelist.keys():
+            self._adjacencyList[a] = []
+            self.set_vertex_value(a, None)
+
+
     def is_directed(self):
+        return self._isdirected
+
+
     def get_vertex_value(self, v):
+        return self._valuelist[v]
+
+
     def set_vertex_value(self, v, x):
+        self._valuelist[v] = x
 
 
 class WeightedGraph(Graph):
+    def __init__(self, startVertex=None):
+        super().__init__(self, startVertex)
+        self._weightDictionary = {}
+
 
     def set_weight(self, a, b, w):
+        pass
+
     def get_weight(self, a, b):
-    	# etc etc
-    	# check the lab assignment for details
+        pass
+
+if __name__ == '__main__':
+    main()
